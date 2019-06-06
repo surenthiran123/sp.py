9a0d94ab87a9fff0f5ae398a9383c895f002ea0d
surenthiran123 <51431710+surenthiran123@users.noreply.github.com>
2019-06-06 12:39:29 +0530

    Update and rename py.py to vowel.py

---
+++ vowel.py
@@ -0,0 +1,9 @@
+X=input()
+if((X>='a')and(X<='z'))or((X>='A')and(X<='Z')):
+     if ((X=='a')or( X=='e')or(X=='i')or(X=='o')or(X=='u')or(X=='A')or(X=='E')or(X=='I')or(X=='O')or(X=='U')):
+         print("Vowel")
+     else:
+         print("Consonant")
+else:
+    print("Invalid")
+    
