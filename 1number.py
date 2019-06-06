cda3bef9ffbbdfc441a2b73fd21c7317e9a9a8d8
surenthiran123 <51431710+surenthiran123@users.noreply.github.com>
2019-06-06 14:51:28 +0530

    Rename number.py to 1number.py

---
+++ 1number.py
@@ -0,0 +1,17 @@
+print("Enter numbers")
+n1=input()
+n2=input()
+n3=input()
+Largest = n1
+if Largest < n2:
+        if n2 > n3:
+            print(n2,"is Largest")
+        else:
+            print(n3,"is Largest")
+elif Largest < n3:
+        if n3 > n2:
+            print(n3,"is Largest")
+        else:
+            print(n2,"is Largest")
+else:
+        print(n1,"is Largest")
