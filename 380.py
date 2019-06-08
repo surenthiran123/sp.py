0b9f1c470f1ff7ef49f40579049ca55144a4b70c
surenthiran123 <51431710+surenthiran123@users.noreply.github.com>
2019-06-08 14:26:37 +0530

    Update 380.py

--- 380.py
+++ 380.py
@@ -1,12 +1,11 @@
 n1,n2=input("").split()
-n1=int(n1)
-n2=int(n2)
+n1,n2=(int(n1),int(n2))
 for i in range(n1,n2):
-     s=0
-     t=i
+     a=0 
+     b=i
      while(i!=0):
           r=i%10;
-          s=s+r*r*r;
+          a=a+r*r*r;
           i//=10;
-     if(t==s):
-        print(t,end=" ")
+     if(b==a):
+        print(b,end=" ")
